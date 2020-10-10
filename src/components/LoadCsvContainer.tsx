1c25a02ae42c4b904e945a054853ebf985d45081
Vaenae <jvannine@gmail.com>
2020-10-10 13:31:30 +0300

    Added React

---
+++ src/components/LoadCsvContainer.tsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function ImportCsvContainer() {
+  return <button id="ImportCsvButton">Import CSV</button>
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import { render } from "react-dom"
+import LoadCsvContainer from "./components/LoadCsvContainer"
+
 const holdingPanelElement = document.getElementById(
   "ctl00_ctl00_MainContent_PM_MainContent_AddHoldingPanel"
 )
-const newElement = document
-  .createElement("div")
-  .appendChild(document.createTextNode("Load CSV"))
+const newElement = document.createElement("div")
+newElement.id = "loadCsvContainer"
 holdingPanelElement?.appendChild(newElement)
+
+render(LoadCsvContainer(), newElement)
