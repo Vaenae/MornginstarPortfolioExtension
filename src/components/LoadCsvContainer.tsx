bb916011290c67609cc9ad73328d03a1ac6d0b12
Vaenae <jvannine@gmail.com>
2020-10-11 19:35:20 +0300

    Load CSV

--- src/components/LoadCsvContainer.tsx
+++ src/components/LoadCsvContainer.tsx
@@ -1,5 +1,20 @@
 import React from "react"
+import Papa from "papaparse"
+
+function loadFile(event: React.ChangeEvent<HTMLInputElement>) {
+  if (event.target.files == null || event.target.files.length < 1) {
+    return
+  }
+  const file = event.target.files[0]
+  Papa.parse(file, {
+    header: true,
+    dynamicTyping: false,
+    complete: results => {
+      console.log(results)
+    }
+  })
+}
 
 export default function ImportCsvContainer() {
-  return <button id="ImportCsvButton">Import CSV</button>
+  return <input type="file" id="ImportCsvButton" onChange={loadFile} />
 }
