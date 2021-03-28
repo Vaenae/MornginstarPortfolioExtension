11babbbc2de85ad2297aac589e3ef5a2bfe5fd00
Vaenae <jvannine@gmail.com>
2021-03-28 10:41:11 +0300

    Store config

--- src/components/LoadCsvModal.tsx
+++ src/components/LoadCsvModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import Papa from "papaparse"
 import Modal from "./bulma/Modal"
 import CsvTable from "./CsvTable"
@@ -14,12 +14,22 @@ interface LoadCsvModalProps {
 
 export default (props: LoadCsvModalProps) => {
   const [config, setConfig] = useState(defaultConfig)
+  useEffect(() => {
+    chrome.storage.sync.get(["importCsvConfig"], result => {
+      if (result.importCsvConfig) {
+        setConfig(result.importCsvConfig)
+      }
+      console.log(`Config loaded: ${JSON.stringify(result)}`)
+    })
+  }, [])
+  const onSave = () => {
+    chrome.storage.sync.set({ importCsvConfig: config }, () => {
+      console.log(`Config saved: ${JSON.stringify(config)}`)
+    })
+    props.updatePortfolio()
+  }
   return (
-    <Modal
-      title="Load csv"
-      onSave={props.updatePortfolio}
-      onCancel={props.cancel}
-    >
+    <Modal title="Load csv" onSave={onSave} onCancel={props.cancel}>
       {isReadonlyNonEmptyArray(props.csvInput.data) ? (
         <div>
           <ImportCsvConfig
