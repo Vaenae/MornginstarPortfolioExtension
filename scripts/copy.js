267923ae75c4ddd0fbfebb32fb4330e62aef57aa
Joonas Vanninen <jvannine@gmail.com>
2021-07-22 16:46:22 +0300

    Create dir if missing

--- scripts/copy.js
+++ scripts/copy.js
@@ -2,6 +2,10 @@ const fs = require("fs")
 
 const fileName = "manifest.json"
 
+if (!fs.existsSync("dist")) {
+  fs.mkdirSync("dist")
+}
+
 fs.createReadStream(`src/${fileName}`).pipe(
   fs.createWriteStream(`dist/${fileName}`)
 )
